d49a08c54ed136c575901a899defc411bb683b25
sergiomartins8 <sergiomartins8.dev@gmail.com>
2020-12-19 16:34:25 +0000

    initial commit

--- src/test/java/io/company/utils/config/CustomConfigurationHolder.java
+++ src/test/java/io/company/utils/config/CustomConfigurationHolder.java
@@ -4,9 +4,15 @@ import io.company.utils.logging.Loggable;
 
 public class CustomConfigurationHolder implements Loggable {
     private final String customConfiguration = System.getProperty("custom.configuration");
+    private final int pipelineID = System.getenv("CI_PIPELINE_ID") == null
+            ? -1 : Integer.parseInt(System.getenv("CI_PIPELINE_ID"));
 
     public String exampleConfiguration() {
         logger().info("Custom config: " + customConfiguration);
         return customConfiguration;
+    }
+
+    public int pipelineID() {
+        return pipelineID;
     }
 }
